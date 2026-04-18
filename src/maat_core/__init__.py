271fb1fc3d9b8b7e5e0032252892a2402687aec7
Chris4081 <christof.krieg@outlook.com>
2026-04-18 18:12:56 +0200

    Refactor __all__ declaration and add __version__

--- src/maat_core/__init__.py
+++ src/maat_core/__init__.py
@@ -1,4 +1,13 @@
 from .core import Field, Constraint, MaatCore
 from .diagnostics import Diagnostics, FieldReport, ConstraintReport
 
-__all__ = ["Field", "Constraint", "MaatCore", "Diagnostics", "FieldReport", "ConstraintReport"]
+__all__ = [
+    "Field",
+    "Constraint",
+    "MaatCore",
+    "Diagnostics",
+    "FieldReport",
+    "ConstraintReport"
+]
+
+__version__ = "0.1.2"
